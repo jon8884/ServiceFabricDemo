80ea6d4fce0209e5d1e889f99d389b25a737a060
Jon Perkins <jonperkins@outlook.com>
2016-12-07 10:16:38 -0800

    Transaction object now extensible

--- TransactionService.Domain/Transaction.cs
+++ TransactionService.Domain/Transaction.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace TransactionService.Domain
 {
-    public class Transaction
+    public class Transaction : IExtensibleDataObject
     {
+        //Forward upgrade compatibility
+        private ExtensionDataObject theData;
+        public virtual ExtensionDataObject ExtensionData
+        {
+            get { return theData; }
+            set { theData = value; }
+        }
+        //Current properties
         public int Suid { get; set; }
         public TransactionType TransactionType { get; set; }
         public string Listing { get; set; }
